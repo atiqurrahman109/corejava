399a98376670ee8314e3621c19f9b3052b2f3e11
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-04-07 23:58:43 +0600

    work

--- rectangle/src/rectangle/Rectangle.java
+++ rectangle/src/rectangle/Rectangle.java
@@ -1,20 +1,21 @@
-
 package rectangle;
 
 import java.util.Scanner;
 
-
 public class Rectangle {
 
- 
     public static void main(String[] args) {
-        Scanner r=new Scanner(System.in);
+        Scanner r = new Scanner(System.in);
+        
         System.out.println("input length");
-        double input=r.nextDouble();
+        double length = r.nextDouble();
+        
         System.out.println("input width");
-        double input2=r.nextDouble();
-        double result=length*width;
-        System.out.println("rectangula size is"+result);
+        double width = r.nextDouble();
+        
+        double result = length * width;
+        
+        System.out.println("rectangula size is" + result);
     }
-    
+
 }
