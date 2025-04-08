db3aba9028a1ca08c69197d699e130fd39e41e98
atiqur rahman <atiqurrahman.ar001@gmail.com>
2025-04-08 19:01:50 +0600

    08-04-205

--- GradeCalculator/src/gradecalculator/GradeCalculator.java
+++ GradeCalculator/src/gradecalculator/GradeCalculator.java
@@ -1,12 +1,15 @@
 
 package gradecalculator;
 
+import java.util.Scanner;
+
 
 public class GradeCalculator {
 
  
     public static void main(String[] args) {
-
+        Scanner s=new Scanner(System.in);
+        float
     }
     
 }
