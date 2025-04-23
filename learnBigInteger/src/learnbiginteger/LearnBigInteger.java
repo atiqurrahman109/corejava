2c1c96cc27a087b13c794ced4693c03f64c235d7
atiqur rahman <atiqurrahman.ar001@gmail.com>
2025-04-23 16:09:19 +0600

    add

---
+++ learnBigInteger/src/learnbiginteger/LearnBigInteger.java
@@ -0,0 +1,23 @@
+
+package learnbiginteger;
+
+import java.math.BigInteger;
+
+public class LearnBigInteger {
+
+  
+    public static void main(String[] args) {
+     
+        
+        BigInteger bi=new BigInteger("36957");
+        BigInteger bi1 =new BigInteger("362");
+        
+        System.out.println(bi.subtract(bi1));
+        
+        
+        
+        
+        
+    }
+    
+}
