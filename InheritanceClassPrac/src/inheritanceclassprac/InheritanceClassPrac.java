0d79c4db9c5b59917032ef1eda2d76cf169b6633
atiqur rahman <atiqurrahman.ar001@gmail.com>
2025-04-24 16:33:47 +0600

    adds

--- InheritanceClassPrac/src/inheritanceclassprac/InheritanceClassPrac.java
+++ InheritanceClassPrac/src/inheritanceclassprac/InheritanceClassPrac.java
@@ -10,14 +10,15 @@ public class InheritanceClassPrac {
     
     public static void main(String[] args) {
    
-         Female f=new Female();
-         
-        Child c=new Child();
-        
-        c.setCell(258);
-        c.setColor("pink");
-        f.setDressName("saree");
-        f.setPrice(5000);
+       
+        Female f=new Female(36, "selo", "yellow", 5849," 125/5", 14587);
+        Child c=new Child("blue");
+     
+       
+        c.setCell(2587486);
+//        c.setColor("black");
+//        f.setDressName("saree");
+//        f.setPrice(5000);
         
         
         
@@ -26,7 +27,9 @@ public class InheritanceClassPrac {
         System.out.println("cell :"+c.getCell());
         System.out.println("color :"+c.getColor());
         System.out.println("dress :"+f.getDressName());
+        System.out.println("price :"+f.getBrand());
         System.out.println("price :"+f.getPrice());
+        System.out.println("price :"+f.getCell());
         
             
     
