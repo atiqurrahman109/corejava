58076ec62657bf82641df756240d3f6d905d169b
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-04-27 12:23:22 +0600

    adds

---
+++ MultiplicationBy2/src/multiplicationby2/MultiplicationBy2.java
@@ -0,0 +1,34 @@
+
+package multiplicationby2;
+
+import java.util.Scanner;
+
+
+public class MultiplicationBy2 {
+
+    public static void main(String[] args) {
+    
+        
+        Scanner s=new Scanner(System.in);
+            int userinput=s.nextInt();
+            
+//            int i=1;
+           
+            
+            for( int i=1; i <=10; i++){
+               
+                int result = userinput*i;
+            
+            }
+        String i = null;
+        int result;
+        
+        
+        System.out.println(userinput + "*" + i + " = " + result);
+                
+        
+        
+        
+    }
+    
+}
